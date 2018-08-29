264d1c5360cb5527fd4ba6efef6c7b7a134fac00
Peter Schrock <peter@miskit.org>
2018-08-28 23:24:36 -0700

    selection of qwerty buttons selects correct letters in phrase

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 const startButton = document.getElementsByClassName('btn__reset')[0];
 const overlay = document.getElementById('overlay');
 
-startButton.addEventListener('mouseup', () => {
+startButton.addEventListener('click', () => {
     overlay.style.display = 'none';
 });
 
@@ -13,7 +13,6 @@ const phrasesArray = ['takes one to know one', 'we are the champions', 'penny fo
 const ul = document.getElementsByTagName('ul')[0];
 
 let randomPhrase = phrasesArray[Math.floor(Math.random() * phrasesArray.length)];
-let phraseLetters;
 
 for (let i = 0; i < randomPhrase.length; i += 1) {
   let li = document.createElement('li');
@@ -28,3 +27,18 @@ for (let i = 0; i < randomPhrase.length; i += 1) {
 
 
 //qwerty selection
+const letterSelection = document.getElementsByClassName('keyrow');
+
+for (let i = 0; i < letterSelection.length; i += 1) {
+  letterSelection[i].addEventListener('click', (event) => {
+    let selection = event.target.textContent;
+    const lettersLi = ul.children;
+    for (let i = 0; i < lettersLi.length; i += 1){
+      if (selection == lettersLi[i].textContent) {
+        lettersLi[i].className += ' show';
+      } else {
+        //minus hearts
+      }
+    }
+  });
+}
