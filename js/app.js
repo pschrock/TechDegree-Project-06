0840c289c6903d6d4e60b03b16ee4d4fa1d666bc
Peter Schrock <peter@miskit.org>
2018-08-30 00:02:30 -0700

    restructure code according to assingment

--- js/app.js
+++ js/app.js
@@ -1,44 +1,74 @@
-
-// start button hides overlay
 const startButton = document.getElementsByClassName('btn__reset')[0];
 const overlay = document.getElementById('overlay');
+
+const qwerty = document.getElementById('qwerty');
+const keyrow = document.getElementsByClassName('keyrow');
+const qwertyButton = document.getElementsByTagName('button');
+
+const phrases = ['takes one to know one', 'we are the champions', 'penny for your thoughts', 'walk in the park', 'best of both worlds'];
+// const phrase = document.getElementById('phrase');
+
+const scoreboard = document.getElementById('scoreboard');
+const triesLi = document.getElementsByClassName('#scoreboard tries');
+
+let missed = 0;
+
+function getRandomPhraseAsArray(arr){
+  let randomPhrase = arr[Math.floor(Math.random() * arr.length)];
+  let phraseCharArray = randomPhrase.split('');
+  return phraseCharArray;
+}
+
+function addPhrasetoDisplay(arr){
+  // do stuff any arr that is passed in, and add to `#phrase ul`
+  for (let i = 0; i < arr.length; ++i) {
+    const ul = document.getElementsByTagName('ul')[0];
+    const li = document.createElement('li');
+    li.textContent = arr[i];
+    if (arr[i] == " ") {
+      li.className = 'space';
+    } else {
+      li.className = 'letter';
+    }
+    ul.appendChild(li);
+  }
+}
+
+const phraseLetters = document.getElementsByClassName('letter');
+let letterFound = null;
 
 startButton.addEventListener('click', () => {
     overlay.style.display = 'none';
 });
 
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhrasetoDisplay(phraseArray);
 
-// array of 5 phrases
-const phrasesArray = ['takes one to know one', 'we are the champions', 'penny for your thoughts', 'walk in the park', 'best of both worlds'];
-const ul = document.getElementsByTagName('ul')[0];
-
-let randomPhrase = phrasesArray[Math.floor(Math.random() * phrasesArray.length)];
-
-for (let i = 0; i < randomPhrase.length; i += 1) {
-  let li = document.createElement('li');
-  li.textContent = randomPhrase[i];
-  if (randomPhrase[i] == " ") {
-    li.className = 'space';
-  } else {
-    li.className = 'letter';
-  }
-  ul.appendChild(li);
-}
-
-
-//qwerty selection
-const letterSelection = document.getElementsByClassName('keyrow');
-
-for (let i = 0; i < letterSelection.length; i += 1) {
-  letterSelection[i].addEventListener('click', (event) => {
-    let selection = event.target.textContent;
-    const lettersLi = ul.children;
-    for (let i = 0; i < lettersLi.length; i += 1){
-      if (selection == lettersLi[i].textContent) {
-        lettersLi[i].className += ' show';
-      } else {
-        //minus hearts
-      }
+qwerty.addEventListener('click', function checkLetter (event) {
+  let letterOfSelection = event.target.textContent;
+  let i = 0;
+  do {
+    if (letterOfSelection == phraseLetters[i].textContent) {
+      phraseLetters[i].className += ' show';
+      letterFound = phraseLetters[i].innerHTML;
+      break;
     }
-  });
-}
+    ++i;
+  } while (i < phraseLetters.length)
+  console.log(letterFound);
+});
+
+window.addEventListener('keypress', function checkLetter (event) {
+  let letterOfSelection = event.key;
+  console.log(letterOfSelection);
+  let i = 0;
+  do {
+    if (letterOfSelection == phraseLetters[i].textContent) {
+      phraseLetters[i].className += ' show';
+      letterFound = phraseLetters[i].innerHTML;
+      break;
+    }
+    ++i;
+  } while (i < phraseLetters.length)
+  console.log(letterFound);
+});
